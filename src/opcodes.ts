ada89eac2cba5e8bc1e5321f613352b01969737b
ismaelvr1999 <ismael.villalpando@alumnos.udg.mx>
2025-11-12 21:23:38 -0600

    Absolute X,Y address and Indirect X,Y tests added

--- src/opcodes.ts
+++ src/opcodes.ts
@@ -1,7 +1,11 @@
 export default {
     INS_LDA_IM: 0xA9, //Load Accumulator Immediate
     INS_LDA_ABS:0xAD, //Load Accumulator Absolute
+    INS_LDA_ABSX:0xBD, //Load Accumulator Absolute X
+    INS_LDA_ABSY:0xB9, //Load Accumulator Absolute Y
     INS_LDA_ZP: 0xA5, //Load Accumulator Zero page
-    INS_LDA_ZPX: 0xB5, //Load Accumulator Zero page x
+    INS_LDA_ZPX: 0xB5, //Load Accumulator Zero page X
+    INS_LDA_INDX: 0XA1, //Load Accumulator Indirect X
+    INS_LDA_INDY: 0XB1, //Load Accumulator Indirect Y
     INS_JSR: 0x20 // Jumb to Subroutine
 };
--- tests/ins_lda.test.ts
+++ tests/ins_lda.test.ts
@@ -46,6 +46,86 @@ export default describe("Instruction LDA test", () => {
         assert.equal(cpu.cycles, 0);
     });
 
+    test("Absolute X Address Mode can load value into the A register", () => {
+        //given
+        const mem = new Memory();
+        const X = 0x01;
+        mem.data[0xFFFC] = opcodes.INS_LDA_ABSX;
+        mem.data[0xFFFD] = 0x80;
+        mem.data[0xFFFE] = 0x44;
+        mem.data[0x4480 + X] = 0x6
+        let cpu = new CPU(mem);
+        cpu.X = X;
+        //when
+        cpu.execute(4);
+        //then
+        assert.equal(cpu.A, 0x6);
+        assert.equal(cpu.Z, 0);
+        assert.equal(cpu.N, 0);
+        VerifyUnmodifiedFlagsFromLDA(cpu);
+        assert.equal(cpu.cycles, 0);
+    });
+
+    test("Absolute X Address Mode can load value into the A register when it crosses a page boundary", () => {
+        //given
+        const mem = new Memory();
+        const X = 0xFF;
+        mem.data[0xFFFC] = opcodes.INS_LDA_ABSX;
+        mem.data[0xFFFD] = 0x02;
+        mem.data[0xFFFE] = 0x44;
+        mem.data[0x4501] = 0x6 // 0x4402 + crosses page boundary
+        let cpu = new CPU(mem);
+        cpu.X = X;
+        //when
+        cpu.execute(5);
+        //then
+        assert.equal(cpu.A, 0x6);
+        assert.equal(cpu.Z, 0);
+        assert.equal(cpu.N, 0);
+        VerifyUnmodifiedFlagsFromLDA(cpu);
+        assert.equal(cpu.cycles, 0);
+    });
+
+    test("Absolute Y Address Mode can load value into the A register", () => {
+        //given
+        const mem = new Memory();
+        const Y = 0x01;
+        mem.data[0xFFFC] = opcodes.INS_LDA_ABSY;
+        mem.data[0xFFFD] = 0x80;
+        mem.data[0xFFFE] = 0x44;
+        mem.data[0x4480 + Y] = 0x6
+        let cpu = new CPU(mem);
+        cpu.Y = Y;
+        //when
+        cpu.execute(4);
+        //then
+        assert.equal(cpu.A, 0x6);
+        assert.equal(cpu.Z, 0);
+        assert.equal(cpu.N, 0);
+        VerifyUnmodifiedFlagsFromLDA(cpu);
+        assert.equal(cpu.cycles, 0);
+    });
+
+    test("Absolute Y Address Mode can load value into the A register when it crosses a page boundary", () => {
+        //given
+        const mem = new Memory();
+        const Y = 0xFF;
+        mem.data[0xFFFC] = opcodes.INS_LDA_ABSY;
+        mem.data[0xFFFD] = 0x02;
+        mem.data[0xFFFE] = 0x44;
+        mem.data[0x4501] = 0x6 // 0x4402 + crosses page boundary
+        let cpu = new CPU(mem);
+        cpu.Y = Y;
+        //when
+        cpu.execute(5);
+        //then
+        assert.equal(cpu.A, 0x6);
+        assert.equal(cpu.Z, 0);
+        assert.equal(cpu.N, 0);
+        VerifyUnmodifiedFlagsFromLDA(cpu);
+        assert.equal(cpu.cycles, 0);
+    });
+
     test("LDA Zero Page Address Mode can load value into the A register", () => {
         const mem = new Memory();
         let zeroPageAddress = 0xFE;
@@ -64,11 +144,13 @@ export default describe("Instruction LDA test", () => {
 
     test("LDA Zero Page X can load value into the A register ", () => {
         const mem = new Memory();
-        let zeroPageAddress = 0x42;
+        const zeroPageAddress = 0x42;
+        const X = 1;
         mem.data[0xFFFC] = opcodes.INS_LDA_ZPX;
         mem.data[0xFFFD] = zeroPageAddress;
-        mem.data[zeroPageAddress] = 0x6; // X = 0 
+        mem.data[zeroPageAddress + X] = 0x6; 
         let cpu = new CPU(mem);
+        cpu.X = X;
         cpu.execute(4);
 
         assert.equal(cpu.A, 0x6);
@@ -80,13 +162,14 @@ export default describe("Instruction LDA test", () => {
 
     test("LDA Zero Page X can load value into the A register when it wraps ", () => {
         const mem = new Memory();
-        let zeroPageAddress = 0x80;
+        const zeroPageAddress = 0x80;
+        const X = 0xFF; 
         mem.data[0xFFFC] = opcodes.INS_LDA_ZPX;
         mem.data[0xFFFD] = zeroPageAddress;
         // 0x80 +  0xFF = 0x017F; 0x017F & 0x00FF = 0x007f;   
         mem.data[0x7F] = 0x37;
         let cpu = new CPU(mem);
-        cpu.X = 0xFF;
+        cpu.X = X;
         cpu.execute(4);
 
         assert.equal(cpu.A, 0x37);
@@ -96,5 +179,66 @@ export default describe("Instruction LDA test", () => {
         assert.equal(cpu.cycles, 0);
     });
 
+    test("Indirect X Address Mode can load value into the A register", () => {
+        //given
+        const mem = new Memory();
+        const X = 0x04;
+        mem.data[0xFFFC] = opcodes.INS_LDA_INDX;
+        mem.data[0xFFFD] = 0x02;
+        mem.data[0x0006] = 0x00;
+        mem.data[0x0007] = 0x80;
+        mem.data[0x8000] = 0x37; 
+        let cpu = new CPU(mem);
+        cpu.X = X;
+        //when
+        cpu.execute(6);
+        //then
+        assert.equal(cpu.A, 0x37);
+        assert.equal(cpu.Z, 0);
+        assert.equal(cpu.N, 0);
+        VerifyUnmodifiedFlagsFromLDA(cpu);
+        assert.equal(cpu.cycles, 0);
+    });
 
+    test("Indirect Y Address Mode can load value into the A register", () => {
+        //given
+        const mem = new Memory();
+        const Y = 0x04;
+        mem.data[0xFFFC] = opcodes.INS_LDA_INDY;
+        mem.data[0xFFFD] = 0x02;
+        mem.data[0x0002] = 0x00;
+        mem.data[0x0003] = 0x80;
+        mem.data[0x8004] = 0x37; //0x8000 + 0x4 
+        let cpu = new CPU(mem);
+        cpu.Y = Y;
+        //when
+        cpu.execute(5);
+        //then
+        assert.equal(cpu.A, 0x37);
+        assert.equal(cpu.Z, 0);
+        assert.equal(cpu.N, 0);
+        VerifyUnmodifiedFlagsFromLDA(cpu);
+        assert.equal(cpu.cycles, 0);
+    });
+
+    test("Indirect Y Address Mode can load value into the A register when it crosses a page boundary", () => {
+        //given
+        const mem = new Memory();
+        const Y = 0xFF;
+        mem.data[0xFFFC] = opcodes.INS_LDA_INDY;
+        mem.data[0xFFFD] = 0x02;
+        mem.data[0x0002] = 0x02;
+        mem.data[0x0003] = 0x80;
+        mem.data[0x8101] = 0x37; //0x8002 + 0xFF 
+        let cpu = new CPU(mem);
+        cpu.Y = Y;
+        //when
+        cpu.execute(6);
+        //then
+        assert.equal(cpu.A, 0x37);
+        assert.equal(cpu.Z, 0);
+        assert.equal(cpu.N, 0);
+        VerifyUnmodifiedFlagsFromLDA(cpu);
+        assert.equal(cpu.cycles, 0);
+    });
 });
